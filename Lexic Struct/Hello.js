b2d66082112146251f3dde18fe4e484badc2e0a7
Steve <falecomstevevaz@gmail.com>
2024-12-04 22:25:15 -0300

    Implementing a basic variables that I practice

---
+++ Lexic Struct/Hello.js
@@ -0,0 +1,18 @@
+// Everything after double forward slashes is a natural language comment.
+// Read the comments carefully: they explain the JavaScript code.
+
+// variable is a symbolic name for a value.
+// Variables are declared with the var keyword:
+var x; // Declares a variable named x.
+
+// Values ​​can be assigned to variables with the = sign
+x = 0; // Now the variable x has the value 0
+x // => 0: A variable is evaluated with its value.
+
+// JavaScript accepts several types of values
+x = 1; // Numbers.
+x = 0.01; // Only one Number type for integers and reals.
+x = "hello world"; // Text strings enclosed in quotes.
+x = 'JavaScript'; // Apostrophes also delimit strings.
+x = true; // Boolean values.
+x = false; // The other boolean value.
