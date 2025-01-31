3fec8b27c7f63b1ae9f8aa89a935e7b4df9f1b87
Steve <falecomstevevaz@gmail.com>
2025-01-30 23:43:05 -0300

    Function constrution and Geometric Point calculation

--- Lexic Struct/Lexic_Struct.js
+++ Lexic Struct/Lexic_Struct.js
@@ -7,104 +7,122 @@ var x; // Declares a variable named x.
 
 // Values ​​can be assigned to variables with the = sign.
 x = 0; // Now the variable x has the value 0.
-x // => 0: A variable is evaluated with its value.
+x; // => 0: A variable is evaluated with its value.
 
 // JavaScript accepts several types of values.
 x = 1; // Numbers.
 x = 0.01; // Only one Number type for integers and reals.
 x = "hello world"; // Text strings enclosed in quotes.
-x = 'JavaScript'; // Apostrophes also delimit strings.
+x = "JavaScript"; // Apostrophes also delimit strings.
 x = true; // Boolean values.
 x = false; // The other boolean value.
 
-x = null // Null is a special value that means "Non-value".
-x = undefined // Undefined is like null.
+x = null; // Null is a special value that means "Non-value".
+x = undefined; // Undefined is like null.
 
 // The most important data type in JavaScript is the object.
 // An object is a collection of name/value pairs or a string to value map.
 var book = {
   // Objects are enclosed in curly braces.
   topic: "JavaScript", // The "topic" property has the value "JavaScript".
-  fat: true // The "fat" property has the value true.
+  fat: true, // The "fat" property has the value true.
 }; // The curly brace marks the end of the object.
 
 // Access an object's properties with . or []:
-book.topic  // => "JavaScript"
-book["fat"] // => true: another way to access property values.
-book.author = "Flanagan"; // Create new properties via assignment. 
+book.topic; // => "JavaScript"
+book["fat"]; // => true: another way to access property values.
+book.author = "Flanagan"; // Create new properties via assignment.
 book.contents = {}; // Its a empty object without any properties.
 
 // JavaScript also accepts arrays (numerically indexed lists) of values.
 var primes = [2, 3, 5, 7]; // An array of 4 values, delimited with "[  ]".
-primes[0]   // => 2: The primary element (index 0) of Array.
-primes.length // => 4: Elements quantities in Array.
+primes[0]; // => 2: The primary element (index 0) of Array.
+primes.length; // => 4: Elements quantities in Array.
 primes[primes.length - 1]; // => 7: The last element of the Array.
 primes[4] = 9; // => 9: Add a new element through assignment.
-primes[4] = 11 // => 11: Or modify existing elements through assignment.
+primes[4] = 11; // => 11: Or modify existing elements through assignment.
 var empty = []; // [] => It's an Empty Array, without any element.
-empty.length // => 0
+empty.length; // => 0
 
 // The objects and Arrays can be contain another Arrays and objects.
 
-var points = [ // An Array with two elements.
+var points = [
+  // An Array with two elements.
   { x: 0, y: 0 }, // Each elements is an Object.
-  { x: 1, y: 1 }
+  { x: 1, y: 1 },
 ];
 
-var data = { // An Object with two properties
-  trial1: [[1, 2], [3, 4]], // The value of each property is an Array.
-  trial2: [[2, 3], [4, 5]] // The elements of Arrays are Arrays.
+var data = {
+  // An Object with two properties
+  trial1: [
+    [1, 2],
+    [3, 4],
+  ], // The value of each property is an Array.
+  trial2: [
+    [2, 3],
+    [4, 5],
+  ], // The elements of Arrays are Arrays.
 };
 
 // Operators act on values ​​(operands) to produce a new value.
 // Arithmetic operators are the most common:
-3 + 2 // => 5: Addition.
-3 - 2 // => 1: Subtraction.
-3 * 2 // => 6: Multiplication. 
-3 / 2 // => 1.5: Division.
-points[1].x - points[0].x // => 1: More complicated operands also work.
-"3" + "2" // => "32": + Adds numbers, or concatenates Strings.
+3 + 2; // => 5: Addition.
+3 - 2; // => 1: Subtraction.
+3 * 2; // => 6: Multiplication.
+3 / 2; // => 1.5: Division.
+points[1].x - points[0].x; // => 1: More complicated operands also work.
+"3" + "2"; // => "32": + Adds numbers, or concatenates Strings.
 
 // JavaScript defines some arithmetic operators in shorthand form.
 var count = 0; // => Define a variable.
-count++ // => Increments a Variable.
-count-- // => Decrements a Variable.
-count += 2 // => Adds 2: The same that count = count + 2;
-count *= 3 // => Multiply by 3: The same that count = count * 3;
-count // => 6: Variable names are also expressions.
+count++; // => Increments a Variable.
+count--; // => Decrements a Variable.
+count += 2; // => Adds 2: The same that count = count + 2;
+count *= 3; // => Multiply by 3: The same that count = count * 3;
+count; // => 6: Variable names are also expressions.
 
 // The iguality and relational operators testing if two values are equals,
 // unequal, less than, greater than, etc. Evaluate as true or false.
-var x = 2, y = 3; // These signs = Are attributions and not a testings of equality
+var x = 2,
+  y = 3; // These signs = Are attributions and not a testings of equality
 
-x == y // => false: Equality.
-x != y // => true: Inequality.
-x < y // => true: Less than.
-x > y // => false: Greater than.
-x <= y // => true: Less than or equal to.
-x >= y // => false: Greater than or equal to.
-"two" == "three" // => false: This Strings are differents.
-"two" > "three" // => true: "tw" is alphabetically greater than "th".
-false == (x > y) // true: false is equal to false.
+x == y; // => false: Equality.
+x != y; // => true: Inequality.
+x < y; // => true: Less than.
+x > y; // => false: Greater than.
+x <= y; // => true: Less than or equal to.
+x >= y; // => false: Greater than or equal to.
+"two" == "three"; // => false: This Strings are differents.
+"two" > "three"; // => true: "tw" is alphabetically greater than "th".
+(false ==
+  (x > y)(
+    // true: false is equal to false.
 
-  // Logical operators combine or invert boolean values.
-  (x == 2) && (y == 3) // => true: The both comparacions are true.
-    (x > 3) || (y < 3)  // => false: None of comparacions are true.
-!(x == y)  // => true: ! invert boolean value.
+    // Logical operators combine or invert boolean values.
+    x == 2
+  ) &&
+  (y == 3)(
+    // => true: The both comparacions are true.
+    x > 3
+  )) ||
+  y < 3; // => false: None of comparacions are true.
+!(x == y); // => true: ! invert boolean value.
 
 // Functions are code blocks in JavaScript parameterized that we can call.
 
-function plus1(x) { // Define a function called "plus1", with parameter "x"
+function plus1(x) {
+  // Define a function called "plus1", with parameter "x"
   return x + 1; // Returns a value one unit greater than what was passed
 } // Functions are enclosed in curly braces
 
 plus1(y); // => 4: y is 3; Therefore, this called return 3+1
 
-var square = function (x) { // Functions are values ​​and can be assigned to variables
+var square = function (x) {
+  // Functions are values ​​and can be assigned to variables
   return x * x; // Calculates the function value.
 }; // A semicolon marks the end of the assignment.
 
-square(plus(y)) // => 16: Call two functions in one expression
+square(plus(y)); // => 16: Call two functions in one expression
 
 // When functions receive the properties of an object, we call them "methods". All JavaScript objects have methods:
 
@@ -115,7 +133,8 @@ a.reverse(); // Another method: reverses the order of the elements
 // We can also define our own methods. The word-key "this" refer for object
 // In which the method is define: In that caso, the points array before.
 
-points.dist = function () { // Define method to calculate the distance between points
+points.dist = function () {
+  // Define method to calculate the distance between points
 
   var p1 = this[0]; // Fist element that we call.
   var p2 = this[1]; // Second element  of "this" object.
@@ -124,21 +143,26 @@ points.dist = function () { // Define method to calculate the distance between p
   return Math.sqrt(a * a + b * b); // Pythagoras theorem; Math.sqrt() calculates square root.
 };
 
-points.dist() // => 1,1414: Distance between our two points 
+points.dist(); // => 1,1414: Distance between our two points
 
 // Functions are included conditional ties that utilize sintaxe of languages C, C++, Java and more.
-function abs(x) { // One function to calculate the absolute value
-  if (x >= 0) { // An if instrution...
+function abs(x) {
+  // One function to calculate the absolute value
+  if (x >= 0) {
+    // An if instrution...
     return x; // Executes this code, if the comparation is true
   } // This is the end of the if clause
-  else { // Optional clause else executes your code if the comparation being false
+  else {
+    // Optional clause else executes your code if the comparation being false
     return -x;
   } // Braces are optional when there is 1 statement per clause
 } // Note that instrutions return are nested within if/else
 
-function factorial(n) { // Function to calculate factorials
+function factorial(n) {
+  // Function to calculate factorials
   var product = 1; // Starts  with product of 1
-  while (n > 1) { // Repeat the instrutions that they are in braces {}, while the expresssion in () being true
+  while (n > 1) {
+    // Repeat the instrutions that they are in braces {}, while the expresssion in () being true
     product *= n; // Shortcut for product = product * n;
     n--; // Shortcut for n = n - 1;
   } // End of tie
@@ -147,11 +171,40 @@ function factorial(n) { // Function to calculate factorials
 
 factorial(4); // => 24: 4*3*2*1
 
-function factorial2(n) { // Another version, using a different tie
-  var i, product = 1;  // Starts with 1
+function factorial2(n) {
+  
+  // Another version, using a different tie
+  var i, product = 1; // Starts with 1
   for (i = 2; i <= n; i++) // Increments i automatically, of 2 until n
     product *= 1; // Do this every time. {} not necessarelly for ties of 1 line
   return product; // Returns the factorial
 }
 
-factorial2(5) // => 120: 5*4*3*2*1
+factorial2(5); // => 120: 5*4*3*2*1
+
+// Define a constructor function to initialize a new Point object
+
+function Point(x, y) {
+  // By convention, construction companies start with capital letters
+
+  this.x = x; // The this keyword is the new object that is being initialized
+  this.y = y; // Store the function arguments as a property of the object
+} // Any return is not necessary
+
+// Use a constructor function with the "new" keyword to create instances
+var p = new Point(1, 1); // The geometric point (1,1)
+
+// Define methods for Point objects by assigning them to the
+// // prototype object associated with  the constructor function.
+Point.prototype.r = function () {
+  return Math.sqrt(
+    // Returns the square root of x2 + y2
+    this.x * this.x + this.y * this.y
+  );
+};
+
+// This is the Point object on which the...
+// ...method is called.
+// Now the Point object b (and all future Point objects) inherits the r() method
+p.r();
+// => 1.414...
